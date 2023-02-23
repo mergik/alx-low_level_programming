4d89e222be59d117dd01314167817ac38f9bf083
MR311X <maker@mgm.anonaddy.me>
2023-02-23 05:06:53 +0300

    Correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,19 @@
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n <= 98)
 	{
-		for (i = n; i < 99; i++)
-			printf("%d, ", i);		
-	}
-	else 
-	{
-		for (i = n; i > 99; i--)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
-	}
 
-	printf("\n");
+		printf("98\n");
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+
+		printf("98\n");
+	}
 }
