15db8d7aaae65e9972077f041dbb42119e381654
MR311X <maker@mgm.anonaddy.me>
2023-03-02 22:14:10 +0300

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * *_strcat - concatenates two strings
+  * @dest: pointer to destination string
+  * @src: pointer to source string
+  * Return: return dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+
+	*p = '\0';
+	return (dest);
+}
