ffeb0c0abe86836a905ea18f1f015c2d75dea7f4
MR311X <maker@mgm.anonaddy.me>
2023-03-03 00:19:12 +0300

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * *_strncpy - copies a string
+  * @dest: pointer to destination string
+  * @src: pointer to source string
+  * @n: number of bytes to use
+  * Return: returns dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	/* If we copied fewer than n characters */ 
+	/* pad the rest of the destination string with null bytes */
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
