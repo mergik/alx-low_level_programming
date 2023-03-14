ca36e5beca1de179f8311bae5e60b1d1063fa858
MR311X <maker@mgm.anonaddy.me>
2023-03-14 17:22:28 +0300

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * isupper - checks if character is uppercase
+  * @c: character to be checked
+  * Return: returns 1 if c is uppercase and 0 if otherwise
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
