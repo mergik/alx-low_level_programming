72e5382b714e13a291e14610c399c92ced321c58
MR311X <maker@mgm.anonaddy.me>
2023-03-14 17:36:57 +0300

    Betty correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,12 @@
   * mul - multiplies two integers
   * @a: integer to be multiplied
   * @b: integer to be multiplied
+  * Return: returns result
   */
 int mul(int a, int b)
 {
 	int result;
 
 	result = a * b;
-	return(result);
+	return (result);
 }
