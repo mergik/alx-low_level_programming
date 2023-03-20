3de9712c79a1eca7dedfe3e7c2bb98a3fe14038c
MR311X <maker@mgm.anonaddy.me>
2023-03-21 02:11:36 +0300

    Correction: check if d is a valid pointer

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,9 +6,13 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the name of the owner of the dog
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL) /* check if d is a valid pointer */
+		return;
+
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
