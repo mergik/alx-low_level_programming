83a592d5f19b60b5ceb4dfdc08e7018df1c6d7f6
MR311X <maker@mgm.anonaddy.me>
2023-03-21 01:42:58 +0300

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ * Description: This struct represents a dog and includes the dog's name, age
+ * and the name of the owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
