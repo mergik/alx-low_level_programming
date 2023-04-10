897a602389e64a8ddc85e661232877d119aef8dd
MR311X <maker@mgm.anonaddy.me>
2023-04-10 23:46:55 +0300

    Betty corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,13 +2,14 @@
 #include <stddef.h>
 
 /**
-  * binary_to_int - convert binary to unsigned integer
+  * binary_to_uint - convert binary to unsigned integer
   * @b: binary to be converted
   * Return: returns converted number or 0 if error
   */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+
 	if (b == NULL)
 		return (0);
 
