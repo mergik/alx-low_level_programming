11a01fdcc224e133a3bc85a85a4b8438c0c277df
MR311X <maker@mgm.anonaddy.me>
2023-04-10 23:44:26 +0300

    0. 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * binary_to_int - convert binary to unsigned integer
+  * @b: binary to be converted
+  * Return: returns converted number or 0 if error
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	if (b == NULL)
+		return (0);
+
+	for (; *b != '\0'; ++b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = (result << 1) + (*b - '0');
+	}
+
+	return (result);
+}
