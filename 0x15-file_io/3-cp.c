3df8eaf0fbed7190cc935b16f43db526d5bdbcc1
MR311X <maker@mgm.anonaddy.me>
2023-05-09 02:57:06 +0300

    Minor betty correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,9 @@ int main(int argc, char *argv[])
 	if (file_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2],
+			O_CREAT | O_WRONLY | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 
