65bdf9235af382a4c46540bad7b87a7de56d03db
MR311X <maker@mgm.anonaddy.me>
2023-08-24 02:03:34 +0300

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains the perimeter function"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid.
+
+    Args:
+    grid (List[List[int]]): A list of lists of integers representing the island.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
