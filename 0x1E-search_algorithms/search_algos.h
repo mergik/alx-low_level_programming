aad620da35aa24e4c5709da025b72c71712633c6
mergik <gmerlix@yahoo.com>
2023-10-10 02:08:00 +0300

    0. Linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+/* BUILT IN HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
